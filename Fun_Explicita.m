41451cd35ba6a4e5ac106e7b6300d48b6fdfaa86
ERNESTO ARCHUNDIA MONTIEL <91717390+ErnestoArchundia@users.noreply.github.com>
2021-11-26 06:26:04 -0600

    Add files via upload

---
+++ Fun_Explicita.m
@@ -0,0 +1,19 @@
+% Octave Script
+% School         : Tecnologico de Estdios Superiores de Jilotepec
+% Date           :20211125
+% Title          :Ejemplo de la funcion Explicita
+% Description    :Script realizacion de problemas y haci identificar que funcion es del mismo
+% Author         :Jennifer,Rubi,Netftali,Angeles,Ernesto y felipe
+% Matricula      : 202123795
+%Grupo           :3101
+% Usage          :octave> /Users/Hola
+%                :octave>Funcion
+% Notes          :Requiere aplicacion octave, usar su linea de comandos
+%                :https://octaveintro.readthedocs.io/en/latest/index.html
+%Problema        : DOS
+
+clear 
+ezplot("(2 * x) + 1",[-10, 10]);
+title ("Función Explicita ");
+xlabel("Dominio (-10, +10)");
+ylabel("Rango(-inf, inf)");
